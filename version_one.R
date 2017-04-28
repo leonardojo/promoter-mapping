65e2bbe5fb9b1740e01d695672f9e8dcc3ce3751
leonardojo <jo.leonardo85@gmail.com>
2017-04-28 15:59:24 -0700

    ver 1.0 completed

--- version_one.R
+++ version_one.R
@@ -3,10 +3,12 @@ options(stringsAsFactors = F)
 
 ## Organizing the table
 table <- read.table('test1.bed.txt', header = F, sep = "\t")
+
 table$V8 <- table$V8 - table$V2
 table$V9 <- table$V9 - table$V2
 
 names(table)[4] <- "Genes"
+
 
 table$motif.start[table$V6 == "+"] <- table[table$V6 == "+", 8]
 table$motif.stop[table$V6 == "+"] <- table[table$V6 == "+", 9]
@@ -24,17 +26,20 @@ ggplot(data = table) +
   geom_point(data = subset(table, (V6 == "+") & (V12 == "-")), aes(x=motif.middle, y= -0.15, fill = V10), shape = 24, size = 3, show.legend = FALSE) + 
   geom_point(data = subset(table, (V6 == "-") & (V12 == "+")), aes(x=motif.middle, y= -0.15, fill = V10), shape = 24, size = 3, show.legend = FALSE) +
   geom_point(data = subset(table, (V6 == "-") & (V12 == "-")), aes(x=motif.middle, y= 0.15, fill = V10), shape = 25, size = 3) +
-  geom_point(aes(x=450,y=0.1, fill = "TSS", shape = "TSS"),shape = 42, size = 6, show.legend = F) +
-  geom_point(data = point, aes(x= X, y =0.01), fill= "grey20", shape = 108, size = 3) +
+  geom_point(data = point, aes(x= X, y =0.0), fill= "grey20", shape = 108, size = 3) +
+
   xlim(1,500) + 
   ylim(-0.5,0.5) +
   theme_bw() +
   coord_fixed(ratio = 100) +
   facet_wrap("Genes", ncol= 3) +
   geom_segment(aes(x=1,xend=500,y=0,yend=0)) +
-  scale_fill_manual(values=c("red2","lightgreen","dodgerblue","grey20")) +
-  scale_shape_manual(values=c(25,25,25,42)) +
-  theme(legend.position = "top", axis.title.y=element_blank(), axis.title.x = element_blank(), axis.text.y=element_blank(),axis.ticks.y=element_blank(),axis.text.x=element_blank(),axis.ticks.x=element_blank(), panel.grid.major = element_blank(), panel.grid.minor = element_blank()) + 
+  scale_fill_manual(values=c("red2","lightgreen","dodgerblue", "grey20")) +
+  scale_shape_manual(values=c(24,25)) +
+  theme(legend.position = "top", legend.title = element_blank(),
+        axis.title.y=element_blank(), axis.title.x = element_blank(), axis.text.y=element_blank(), 
+        axis.ticks.y=element_blank(),axis.text.x=element_blank(),axis.ticks.x=element_blank(), 
+        panel.grid.major = element_blank(), panel.grid.minor = element_blank()) + 
   ggsave("Test.jpeg", width = 10, height = 10)
 
 
