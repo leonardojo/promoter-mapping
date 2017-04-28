e86deaa222b17955c922084a5ce6567dd38f3ba8
leonardojo <jo.leonardo85@gmail.com>
2017-04-28 13:33:30 -0700

    Updated

--- version_one.R
+++ version_one.R
@@ -16,13 +16,16 @@ table$motif.stop[table$V6 == "-"] <- (table[table$V6 == "-", 3]-table[table$V6 =
 table$motif.middle <- table$motif.start + as.numeric((table$motif.stop-table$motif.start)/2)
 
 head(table)
-
+point <- data.frame(c(1,100,200,300,400,500))
+names(point) <- "X"
+ 
 ggplot(data = table) + 
-  geom_point(data = subset(table, (V6 == "+") & (V12 == "+")), aes(x=motif.middle, y= 0.2, fill = V10), shape = 25, size = 3) + 
-  geom_point(data = subset(table, (V6 == "+") & (V12 == "-")), aes(x=motif.middle, y= -0.2, fill = V10), shape = 24, size = 3, show.legend = FALSE) + 
-  geom_point(data = subset(table, (V6 == "-") & (V12 == "+")), aes(x=motif.middle, y= -0.2, fill = V10), shape = 24, size = 3, show.legend = FALSE) +
-  geom_point(data = subset(table, (V6 == "-") & (V12 == "-")), aes(x=motif.middle, y= 0.2, fill = V10), shape = 25, size = 3) +
-  geom_point(aes(x= 450,y =0.01), fill= "grey20", shape = 108, size = 3) +
+  geom_point(data = subset(table, (V6 == "+") & (V12 == "+")), aes(x=motif.middle, y= 0.15, fill = V10), shape = 25, size = 3) + 
+  geom_point(data = subset(table, (V6 == "+") & (V12 == "-")), aes(x=motif.middle, y= -0.15, fill = V10), shape = 24, size = 3, show.legend = FALSE) + 
+  geom_point(data = subset(table, (V6 == "-") & (V12 == "+")), aes(x=motif.middle, y= -0.15, fill = V10), shape = 24, size = 3, show.legend = FALSE) +
+  geom_point(data = subset(table, (V6 == "-") & (V12 == "-")), aes(x=motif.middle, y= 0.15, fill = V10), shape = 25, size = 3) +
+  geom_point(aes(x=450,y=0.1, fill = "TSS", shape = "TSS"),shape = 42, size = 6, show.legend = F) +
+  geom_point(data = point, aes(x= X, y =0.01), fill= "grey20", shape = 108, size = 3) +
   xlim(1,500) + 
   ylim(-0.5,0.5) +
   theme_bw() +
@@ -30,7 +33,8 @@ ggplot(data = table) +
   facet_wrap("Genes", ncol= 3) +
   geom_segment(aes(x=1,xend=500,y=0,yend=0)) +
   scale_fill_manual(values=c("red2","lightgreen","dodgerblue","grey20")) +
-  theme(axis.title.y=element_blank(), axis.title.x = element_blank(), axis.text.y=element_blank(),axis.ticks.y=element_blank(), panel.grid.major = element_blank(), panel.grid.minor = element_blank()) + 
+  scale_shape_manual(values=c(25,25,25,42)) +
+  theme(legend.position = "top", axis.title.y=element_blank(), axis.title.x = element_blank(), axis.text.y=element_blank(),axis.ticks.y=element_blank(),axis.text.x=element_blank(),axis.ticks.x=element_blank(), panel.grid.major = element_blank(), panel.grid.minor = element_blank()) + 
   ggsave("Test.jpeg", width = 10, height = 10)
 
 
